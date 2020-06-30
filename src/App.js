873f63e77900bc56fc95b2a411134213ddec990b
Jessica <jess.lee0124@gmail.com>
2020-06-30 17:08:33 -0400

    Add reducers and actions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
-
+import { Provider } from 'react-redux';
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
+import store from './store';
 
 function App() {
   return (
-    <div className="App">
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
